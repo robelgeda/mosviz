74a267e794cbfc328d21e557c6f235e6fbc5bbdb
robelgeda <robelgeda@yahoo.com>
2018-05-16 20:59:16 -0400

    Fix glue compat

--- mosviz/compat.py
+++ mosviz/compat.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import, division, print_function
 
-from glue.core.qt.data_combo_helper import ComponentIDComboHelper as OriginalComponentIDComboHelper
+from glue.core.data_combo_helper import ComponentIDComboHelper as OriginalComponentIDComboHelper
 from glue.core.hub import HubListener
 
 __all__ = ['ComponentIDComboHelper']
